81ecbb48555501a43d285eb081662c8e0863b35f
Michael Watson <miwats@michaelw-mac.local>
2017-09-28 10:03:08 -0700

    Add Video Preview Effect on Android

--- Mobile App/Droid/Effects/VideoPreviewEffect.cs
+++ Mobile App/Droid/Effects/VideoPreviewEffect.cs
@@ -1,10 +1,61 @@
-﻿using System;
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+using ICC.Droid;
+using Android.Graphics;
+using Android.Media;
+using System;
+using Android.OS;
+using System.Collections.Generic;
+using Android.Widget;
+
+[assembly: ResolutionGroupName("Xamarin")]
+[assembly: ExportEffect(typeof(VideoPreviewEffect), "VideoPreviewEffect")]
 namespace ICC.Droid
 {
-	public class VideoPreviewEffect
+	public class VideoPreviewEffect : PlatformEffect
 	{
-		public VideoPreviewEffect()
+		protected override void OnAttached()
 		{
+			UpdateUI();
+		}
+
+		protected override void OnDetached()
+		{
+		}
+
+		protected override void OnElementPropertyChanged(System.ComponentModel.PropertyChangedEventArgs args)
+		{
+			base.OnElementPropertyChanged(args);
+
+			switch (args.PropertyName)
+			{
+				case nameof(VideoImagePreview):
+					UpdateUI();
+					break;
+			}
+		}
+
+		void UpdateUI()
+		{
+			var uri = ((VideoImagePreview)Element).VideoUrl;
+			MediaMetadataRetriever mediaMetadataRetriever = new MediaMetadataRetriever();
+
+			try
+			{
+				mediaMetadataRetriever.SetDataSource(uri, new Dictionary<string, string>());
+
+				using (var bitmap = mediaMetadataRetriever.GetFrameAtTime(0))
+					((ImageView)Control).SetImageBitmap(bitmap);
+			}
+			catch (Exception e)
+			{
+				System.Diagnostics.Debug.WriteLine(e.Message);
+			}
+			finally
+			{
+				mediaMetadataRetriever?.Release();
+			}
 		}
 	}
 }
